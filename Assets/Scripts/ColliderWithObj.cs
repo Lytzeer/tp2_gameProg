5743975fff1e817c79b378aeae3509c6c3a8e80f
Matsuel <matsuel0308@gmail.com>
2024-01-15 15:08:41 +0100

    add gameover

--- Assets/Scripts/ColliderWithObj.cs
+++ Assets/Scripts/ColliderWithObj.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColliderWithObj : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class ColliderWithObj : MonoBehaviour
         if (other.gameObject.tag == "Obstacle")
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(1);
         }
     }
 }
