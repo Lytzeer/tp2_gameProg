29ad9c5f3d5715abe1d6fc0d5ccc9d1993811859
Matsuel <matsuel0308@gmail.com>
2024-01-15 16:46:00 +0100

    add rotaion and patch

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public GameObject jumper;
     public float jumpForce = 10f;
     public bool onFloor = false;
     // Start is called before the first frame update
@@ -16,9 +17,25 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Space)&& onFloor)
+        if (Input.GetKeyUp(KeyCode.Space) && onFloor)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            StartCoroutine(RotateJumper(jumper, 1.0f));
+
         }
+    }
+
+    IEnumerator RotateJumper(GameObject target, float duration)
+    {
+        Quaternion startRotation = target.transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, 0, 180);
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            target.transform.rotation = Quaternion.Lerp(startRotation, endRotation, t / duration);
+            yield return null;
+        }
+
+        target.transform.rotation = endRotation;
     }
 }
