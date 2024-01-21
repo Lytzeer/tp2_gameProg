9d53f746871a112952bc04a526eb2b69edd1458b
Matsuel <matsuel0308@gmail.com>
2024-01-21 13:12:34 +0100

    speed grow all 10 points

--- Assets/Scripts/CreateObstacle.cs
+++ Assets/Scripts/CreateObstacle.cs
@@ -6,7 +6,7 @@ public class CreateObstacle : MonoBehaviour
 {
     public GameObject obstacle;
     public bool canCreate = true;
-    public float YPosition= -4.1f;
+    public float YPosition= -3.1f;
     public int score = 0;
     public List<GameObject> obstacles = new List<GameObject>();
     public List<GameObject> obstaclesToRemove = new List<GameObject>();
@@ -40,6 +40,11 @@ public class CreateObstacle : MonoBehaviour
     {
         while (canCreate)
         {
+            float speed = -5f;
+            if (score%10==0 && score!=0)
+            {
+                speed -= 1f;
+            }
             float randomTime = Random.Range(1.5f, 2.5f);
             yield return new WaitForSeconds(randomTime);
 
@@ -51,16 +56,16 @@ public class CreateObstacle : MonoBehaviour
                 {
                     yield return new WaitForSeconds(0.25f);
                     GameObject newObstacle = Instantiate(obstacle);
-                    newObstacle.transform.position = new Vector2(10f, YPosition);
-                    newObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(-5f, 0f);
+                    newObstacle.transform.position = new Vector2(10f, YPosition-1.0f);
+                    newObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0f);
                     obstacles.Add(newObstacle);
                 }
             }else{
-                float scale = Random.Range(0.5f, 1.5f);
+                float scale = Random.Range(0.8f, 1.5f);
                 GameObject newObstacle = Instantiate(obstacle);
-                newObstacle.transform.position = new Vector2(10f, YPosition);
-                newObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(-6f, 0f);
+                newObstacle.transform.position = new Vector2(10f, YPosition-scale);
                 newObstacle.transform.localScale = new Vector2(scale, scale);
+                newObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0f);
                 obstacles.Add(newObstacle);
             }
         }
